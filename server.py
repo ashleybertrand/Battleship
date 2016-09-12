879e9da618d673841f876516112f076dad2ed49e
meganweller21 <mweller213@gmail.com>
2016-09-11 19:25:23 -0600

    Change print statement for consistency

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('localhost', 5000)
 BUFFER_SIZE = 20
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print "starting up on %s port %s" % server_address
 sock.bind(server_address)
 sock.listen(1)
 
