8e7172f420b3be02aa37ad2025b7944015206bd5
meganweller21 <mweller213@gmail.com>
2016-09-11 19:20:22 -0600

    Connection working for client and server. Run both server.py and client.py in terminal to send the message

--- server.py
+++ server.py
@@ -8,27 +8,27 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('localhost', 5000)
+BUFFER_SIZE = 20
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
-
 sock.listen(1)
 
+
+print "waiting for a connection"
+connection, client_address = sock.accept()
+
+
+print "connection from ", client_address
+
 while True:
-	print >>sys.stderr, 'waiting for a connection'
-	connection, client_address = sock.accept()
+	data = connection.recv(BUFFER_SIZE)
+	print "received ", data
+	if data:
+		print "sending data back to the client"
+		connection.send(data)
+	else:
+		print "no more data from", client_address
+		break
 
-try:
-	print >>sys.stderr, 'connection from', client_address
 
-	while True:
-		data = connection.recv(16)
-		print >>sys.stderr, 'received "%s"' % data
-		if data:
-			print >>sys.stderr, 'sending data back to the client'
-			connection.sendall(data)
-		else:
-			print >>sys.stderr, 'no more data from', client_address
-			break
-
-finally:
-	connection.close()
+connection.close()
