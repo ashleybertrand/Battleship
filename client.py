f92d84136928014b79937cba31eb219c66ac5944
ashleybertrand <ashleykaybertrand@gmail.com>
2016-09-18 11:36:45 -0600

    Changes

--- client.py
+++ client.py
@@ -22,30 +22,3 @@ print response.status, response.reason
 
 print "closing connection"
 conn.close()
-
-
-'''
-
-#HTTP implementation
-
-#works but is for GET not POST
-"""
-import httplib, urllib
-conn = httplib.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print r1.status, r1.reason
-data1 = r1.read()
-conn.close()
-"""
-
-import httplib, urllib
-params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
-headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-conn = httplib.HTTPConnection("musi-cal.mojam.com:80")
-conn.request("POST", "/cgi-bin/query", params, headers)
-response = conn.getresponse()
-print response.status, response.reason
-data = response.read()
-conn.close()
-'''
